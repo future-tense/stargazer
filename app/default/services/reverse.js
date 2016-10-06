84818a14adf0c4c935e7f01d48e0427bbcd8340d
Johan Stén <johan.sten@gmail.com>
2016-10-06 20:38:47 +0800

    Create a reverse address lookup service

---
+++ app/default/services/reverse.js
@@ -0,0 +1,70 @@
+/* global angular, StellarSdk */
+
+angular.module('app')
+.factory('Reverse', function ($q, Contacts, Wallet) {
+	'use strict';
+
+	var reverseFederationCache = {};
+
+	function reverseFederationLookup(accountId) {
+
+		return Wallet.current.horizon().accounts()
+		.accountId(accountId).call()
+		.then(function (accountInfo) {
+			if (accountInfo.home_domain) {
+				return StellarSdk.FederationServer.createForDomain(accountInfo.home_domain)
+				.then(function (federationServer) {
+					return federationServer.resolveAccountId(accountId)
+					.then(
+						function (res) {
+							return res.stellar_address;
+						},
+						function (err) {
+							return $q.reject(err);
+						}
+					);
+				});
+			} else {
+				return $q.reject();
+			}
+		});
+	}
+
+	function lookup(accountId, network) {
+
+		return $q(function(resolve, reject) {
+			if (accountId in Wallet.accounts) {
+				resolve(Wallet.accounts[accountId].alias);
+			}
+
+			else {
+				var name = Contacts.lookup(accountId, network);
+				if (name) {
+					resolve(name);
+				}
+
+				else if (accountId in reverseFederationCache) {
+					resolve(reverseFederationCache[accountId]);
+				}
+
+				else {
+					reverseFederationLookup(accountId)
+					.then(
+						function (name) {
+							reverseFederationCache[accountId] = name;
+							resolve(name);
+						},
+						function (err) {
+							reject(err);
+						}
+					);
+				}
+			}
+		});
+	}
+
+	return {
+		lookup: lookup
+	};
+
+});
