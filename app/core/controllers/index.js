0d1067b6746a57ab85b2bc27fa56447a19d55f99
Johan Stén <johan.sten@gmail.com>
2017-04-08 15:14:50 +0800

    Break out QR commands to a service

--- app/core/controllers/index.js
+++ app/core/controllers/index.js
@@ -1,118 +1,20 @@
 /* global angular, console, StellarSdk */
 
 angular.module('app')
-.controller('IndexCtrl', function ($http, $ionicBody, $ionicLoading, $ionicPopup, $location, $q, $rootScope, $scope, $translate, $window, Contacts, Horizon, Keychain, Language, Modal, Storage, Wallet) {
+.controller('IndexCtrl', function ($ionicBody, $ionicPopup, $rootScope, $scope, $translate, $window, Commands) {
 	'use strict';
 
-	$scope.onQrCodeScanned = onQrCodeScanned;
-	$scope.physicalScreenWidth = $window.innerWidth;
+	$scope.onQrCodeScanned		= Commands.onQrCodeScanned;
+	$scope.physicalScreenWidth	= $window.innerWidth;
 
 	$rootScope.$on('$submitter.failed', onSubmitterFailed);
-
 	angular.element($window).bind('resize', updateWidth);
-
-	function handleAccountImport(account, key) {
-
-		var data = window.btoa(JSON.stringify({
-			account: account,
-			key: key
-		}));
-		$location.path('/side-menu/import-account/' + data);
-	}
-
-	function handleAccount(account) {
-
-		if (!(account.id in Wallet.accounts) && !Contacts.lookup(account.id, account.network)) {
-
-			$scope.model = {
-				id:			account.id,
-				meta:		account.meta,
-				meta_type:	account.meta_type
-			};
-
-			if (account.network) {
-				$scope.model.network = account.network;
-			}
-
-			Modal.show('app/account/modals/add-contact.html', $scope);
-		}
-	}
-
-	function handlePayment(payment) {
-		var object = {
-			destination:	payment.destination,
-			amount:			payment.amount
-		};
-
-		if (!payment.asset) {
-			object.asset_type	= 'native';
-		} else {
-			object.asset_code	= payment.asset.code;
-			object.asset_issuer	= payment.asset.issuer;
-		}
-
-		if (payment.memo) {
-			object.memo = payment.memo;
-		}
-
-		$location.path('/account/send')
-		.search(object);
-	}
-
-	function handleChallenge(challenge) {
-
-		var id = challenge.id ? challenge.id : Wallet.current.id;
-
-		if (Keychain.isLocalSigner(id)) {
-			Keychain.signMessage(id, challenge.message)
-			.then(function (sig) {
-
-				$ionicLoading.show({
-					template: 'Submitting response...'
-				});
-
-				$http.post(challenge.url, {
-					id: id,
-					msg: challenge.message,
-					sig: sig
-				})
-				.then(function (res) {
-					$ionicLoading.hide();
-				}, function (err) {
-					$ionicLoading.hide();
-				});
-			});
-		}
-	}
-
-	function onQrCodeScanned(data) {
-
-		data = JSON.parse(data);
-		if (!data.stellar) {
-			return;
-		}
-
-		if (data.stellar.account) {
-			if (data.stellar.key) {
-				handleAccountImport(data.stellar.account, data.stellar.key);
-			} else {
-				handleAccount(data.stellar.account);
-			}
-		}
-
-		else if (data.stellar.payment) {
-			handlePayment(data.stellar.payment);
-		}
-
-		else if (data.stellar.challenge) {
-			handleChallenge(data.stellar.challenge);
-		}
-	}
 
 	function onSubmitterFailed(event, err) {
 		$ionicPopup.alert({
 			title: $translate.instant(err)
-		}).then(function () {
+		})
+		.then(function () {
 			//	:KLUDGE: ionic 1.3.2 messes up, so we have to manually remove this
 			$ionicBody.removeClass('modal-open');
 		});
---
+++ app/core/services/commands.js
@@ -0,0 +1,107 @@
+/* global angular */
+
+angular.module('app')
+.factory('Commands', function ($http, $ionicLoading, $location, $rootScope, Contacts, Keychain, Modal, Wallet) {
+	'use strict';
+
+	return {
+		onQrCodeScanned: onQrCodeScanned
+	};
+
+	function onQrCodeScanned(data) {
+
+		data = JSON.parse(data);
+		if (!data.stellar) {
+			return;
+		}
+
+		if (data.stellar.account) {
+			if (data.stellar.key) {
+				handleAccountImport(data.stellar.account, data.stellar.key);
+			} else {
+				handleAccount(data.stellar.account);
+			}
+		}
+
+		else if (data.stellar.payment) {
+			handlePayment(data.stellar.payment);
+		}
+
+		else if (data.stellar.challenge) {
+			handleChallenge(data.stellar.challenge);
+		}
+	}
+
+	function handleAccountImport(account, key) {
+
+		var data = window.btoa(JSON.stringify({
+			account: account,
+			key: key
+		}));
+		$location.path('/side-menu/import-account/' + data);
+	}
+
+	function handleAccount(account) {
+
+		if (!(account.id in Wallet.accounts) && !Contacts.lookup(account.id, account.network)) {
+
+			var scope = $rootScope.$new();
+			scope.model = {
+				id:			account.id,
+				meta:		account.meta,
+				meta_type:	account.meta_type
+			};
+
+			if (account.network) {
+				scope.model.network = account.network;
+			}
+
+			Modal.show('app/account/modals/add-contact.html', scope);
+		}
+	}
+
+	function handlePayment(payment) {
+		var object = {
+			destination:	payment.destination,
+			amount:			payment.amount
+		};
+
+		if (!payment.asset) {
+			object.asset_type	= 'native';
+		} else {
+			object.asset_code	= payment.asset.code;
+			object.asset_issuer	= payment.asset.issuer;
+		}
+
+		if (payment.memo) {
+			object.memo = payment.memo;
+		}
+
+		$location.path('/account/send')
+		.search(object);
+	}
+
+	function handleChallenge(challenge) {
+
+		var id = challenge.id ? challenge.id : Wallet.current.id;
+
+		if (Keychain.isLocalSigner(id)) {
+			Keychain.signMessage(id, challenge.message)
+			.then(function (sig) {
+
+				$ionicLoading.show({
+					template: 'Submitting response...'
+				});
+
+				$http.post(challenge.url, {
+					id: id,
+					msg: challenge.message,
+					sig: sig
+				})
+				.finally(function () {
+					$ionicLoading.hide();
+				});
+			});
+		}
+	}
+});
