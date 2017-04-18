db37c51cb7607638c96fa21d74701aa9e858ddb2
Johan Stén <johan.sten@gmail.com>
2017-04-18 13:34:28 +0800

    Break out the signing request code into a directive

--- app/account/controllers/overview.js
+++ app/account/controllers/overview.js
@@ -1,39 +1,24 @@
 /* global angular, console, toml */
 
 angular.module('app')
-.controller('OverviewCtrl', function ($q, $route, $scope, Reviewer, Transactions, Wallet) {
+.controller('OverviewCtrl', function ($route, $scope, Wallet) {
 	'use strict';
 
-	const accountId = $route.current.params.accountId;
-	if (accountId) {
-		Wallet.current = Wallet.accounts[accountId];
+	$scope.isActivated	= isActivated;
+	$scope.getAssets	= getAssets;
+	$scope.doRefresh	= doRefresh;
+	$scope.wallet		= Wallet;
+
+	activate();
+
+	function activate() {
+		const accountId = $route.current.params.accountId;
+		if (accountId) {
+			Wallet.current = Wallet.accounts[accountId];
+		}
 	}
 
-	$scope.wallet = Wallet;
-	$scope.pending = Transactions;
-
-	$scope.pubkey = Wallet.current.id;
-
-	$scope.isActivated = function () {
-		return Wallet.current.getNativeBalance() !== '0';
-	};
-
-	$scope.isSigned = function (tx, pubkey) {
-		return (tx.hasSigned && pubkey in tx.hasSigned)? 'Signed' : 'Unsigned';
-	};
-
-	$scope.getAssets = function () {
-
-		return Wallet.current.balances.filter(function (e) {
-			if (e.asset_type === 'native') {
-				return true;
-			} else {
-				return (e.balance !== '0.0000000');
-			}
-		});
-	};
-
-	$scope.doRefresh = function () {
+	function doRefresh() {
 
 		Wallet.current.refresh()
 		.then(function () {
@@ -43,13 +28,20 @@ angular.module('app')
 			$scope.$broadcast('scroll.refreshComplete');
 			// :TODO: Display some message about not being able to refresh
 		});
-	};
+	}
 
-	$scope.review = reviewPending;
+	function getAssets() {
 
-	function reviewPending(context) {
-		$q.when(context)
-		.then(Reviewer.review)
-		.catch(function (){});
+		return Wallet.current.balances.filter(function (e) {
+			if (e.asset_type === 'native') {
+				return true;
+			} else {
+				return (e.balance !== '0.0000000');
+			}
+		});
+	}
+
+	function isActivated() {
+		return Wallet.current.getNativeBalance() !== '0';
 	}
 });
---
+++ app/account/directives/account-requests.js
@@ -0,0 +1,38 @@
+/* global angular, console */
+
+angular.module('app')
+.directive('accountRequests', function ($q, Reviewer, Transactions, Wallet) {
+	'use strict';
+
+	return {
+		restrict: 'E',
+		link: link,
+		templateUrl: 'app/account/templates/account-requests.html'
+	};
+
+	function link(scope, element, attributes) {
+		scope.isSigned		= isSigned;
+		scope.review		= reviewPending;
+		scope.getRequests	= getRequests;
+		scope.hasPending	= hasPending;
+		scope.pubkey		= Wallet.current.id;
+	}
+
+	function getRequests(pubkey) {
+		return Transactions.forSigner(pubkey);
+	}
+
+	function hasPending(pubkey) {
+		return getRequests(pubkey).length !== 0;
+	}
+
+	function isSigned(tx, pubkey) {
+		return (tx.hasSigned && pubkey in tx.hasSigned)? 'Signed' : 'Unsigned';
+	}
+
+	function reviewPending(context) {
+		$q.when(context)
+		.then(Reviewer.review)
+		.catch(function (){});
+	}
+});
