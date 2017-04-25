f5483f062021a5d9bbf14e7d8ba6dd8d840b52e2
Johan Stén <johan.sten@gmail.com>
2017-04-25 23:03:46 +0800

    Create an index component, as the main top-level component

---
+++ app/core/components/index.js
@@ -0,0 +1,26 @@
+/* global angular */
+
+(function () {
+	'use strict';
+
+	class IndexController {
+		constructor($window, Commands) {
+			this.physicalScreenWidth = $window.innerWidth;
+			this.Commands = Commands;
+
+			angular.element($window).bind('resize', () => {
+				this.physicalScreenWidth = $window.innerWidth;
+			});
+		}
+
+		onQrCodeScanned(data) {
+			this.Commands.onQrCodeScanned(data);
+		}
+	}
+
+	angular.module('app')
+	.component('index', {
+		controller: IndexController,
+		templateUrl: 'app/core/templates/index.html'
+	});
+}());
