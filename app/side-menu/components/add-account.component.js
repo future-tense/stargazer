b2e5ffdd14a677dd62f235d0c91263d70196d2fa
Johan Stén <johan.sten@gmail.com>
2018-02-18 23:21:44 +0800

    Add back support for shared accounts, and add import from QR

--- app/side-menu/components/add-account.component.js
+++ app/side-menu/components/add-account.component.js
@@ -3,8 +3,32 @@
 (function () {
 	'use strict';
 
+	class AddAccountController {
+		constructor(Commands, QRScanner, Wallet) {
+			this.Commands = Commands;
+			this.QRScanner = QRScanner;
+
+			this.isInitialized = Wallet.accountList.length !== 0;
+
+			if (this.isInitialized) {
+				this.heading = 'sidemenu.account';
+				this.done = 'global.done';
+			} else {
+				this.heading = 'Create an Account';
+				this.done = '';
+			}
+		}
+
+		importFromQR() {
+			this.QRScanner.open()
+			.then(this.Commands.onQrCodeScanned);
+		}
+	}
+
 	angular.module('app')
 	.component('addAccount', {
+		controller: AddAccountController,
+		controllerAs: 'vm',
 		templateUrl: 'app/side-menu/components/add-account.html'
 	});
 }());
