fcb271a7248621f29d2deb1400b827d6b8643643
Johan Stén <johan.sten@gmail.com>
2018-02-18 23:27:52 +0800

    Add a Ledger Nano service

---
+++ app/core/services/ledger-nano.js
@@ -0,0 +1,44 @@
+/* global angular */
+
+angular.module('app')
+.factory('LedgerNano', function ($q, platformInfo) {
+	'use strict';
+
+	let StellarLedger;
+	if (platformInfo.isElectron) {
+		const electron = require('electron');
+		StellarLedger = electron.remote.require('stellar-ledger-api');
+	}
+
+	const bip32Path = (index) => `44'/148'/${index}'`;
+
+	const wrapper = (func, field) => $q((resolve, reject) => {
+		StellarLedger.comm.create_async()
+		.then(comm => {
+			const api = new StellarLedger.Api(comm);
+			func(api)
+			.then(result => resolve(result[field]))
+			.catch(err => reject(err))
+			.done(() => comm.close_async());
+		})
+		.catch(err => reject(err));
+	});
+
+	const getPublicKey = (index) => {
+		const func = api => api.getPublicKey_async(bip32Path(index))
+		return wrapper(func, 'publicKey');
+	};
+
+	const signTxHash = (index, txHash) => {
+		const func = api => api.signTxHash_async(bip32Path(index), txHash);
+		return wrapper(func, 'signature');
+	};
+
+	//			No Device
+	//			Invalid status 6d00				wrong app
+
+	return {
+		getPublicKey: getPublicKey,
+		signTxHash: signTxHash
+	};
+});
