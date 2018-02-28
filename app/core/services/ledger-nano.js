5e1316ee48816cab9c2d8eec9c31b5054d0d801a
Johan Stén <johan.sten@gmail.com>
2018-02-28 19:32:24 +0800

    Use native Promise instead of $q

--- app/core/services/ledger-nano.js
+++ app/core/services/ledger-nano.js
@@ -1,47 +1,41 @@
-/* global angular, require */
+/* global require */
 
-import 'ionic-sdk/release/js/ionic.bundle';
 import platformInfo from './platform-info.js';
 
-angular.module('app.service.ledger-nano', [])
-.factory('LedgerNano', function ($q) {
-	'use strict';
+let StellarLedger;
+if (platformInfo.isElectron) {
+	const electron = require('electron');
+	StellarLedger = electron.remote.require('stellar-ledger-api');
+}
 
-	let StellarLedger;
-	if (platformInfo.isElectron) {
-		const electron = require('electron');
-		StellarLedger = electron.remote.require('stellar-ledger-api');
-	}
+const bip32Path = (index) => `44'/148'/${index}'`;
 
-	const bip32Path = (index) => `44'/148'/${index}'`;
-
-	const wrapper = (func, field) => $q((resolve, reject) => {
-		StellarLedger.comm.create_async()
-		.then(comm => {
-			const api = new StellarLedger.Api(comm);
-			func(api)
-			.then(result => resolve(result[field]))
-			.catch(err => reject(err))
-			.done(() => comm.close_async());
-		})
-		.catch(err => reject(err));
-	});
-
-	const getPublicKey = (index) => {
-		const func = api => api.getPublicKey_async(bip32Path(index))
-		return wrapper(func, 'publicKey');
-	};
-
-	const signTxHash = (index, txHash) => {
-		const func = api => api.signTxHash_async(bip32Path(index), txHash);
-		return wrapper(func, 'signature');
-	};
-
-	//			No Device
-	//			Invalid status 6d00				wrong app
-
-	return {
-		getPublicKey: getPublicKey,
-		signTxHash: signTxHash
-	};
+const wrapper = (func, field) => new Promise((resolve, reject) => {
+	StellarLedger.comm.create_async()
+	.then(comm => {
+		const api = new StellarLedger.Api(comm);
+		func(api)
+		.then(result => resolve(result[field]))
+		.catch(err => reject(err))
+		.done(() => comm.close_async());
+	})
+	.catch(err => reject(err));
 });
+
+const getPublicKey = (index) => {
+	const func = api => api.getPublicKey_async(bip32Path(index));
+	return wrapper(func, 'publicKey');
+};
+
+const signTxHash = (index, txHash) => {
+	const func = api => api.signTxHash_async(bip32Path(index), txHash);
+	return wrapper(func, 'signature');
+};
+
+//			No Device
+//			Invalid status 6d00				wrong app
+
+export default {
+	getPublicKey: getPublicKey,
+	signTxHash: signTxHash
+};
