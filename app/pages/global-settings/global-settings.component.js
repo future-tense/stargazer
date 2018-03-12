997a555ce895e31c5e573922aa9d0ac15d5f1015
Johan Stén <johan.sten@gmail.com>
2018-03-12 16:41:47 +0800

    Add back advanced mode setting

--- app/pages/global-settings/global-settings.component.js
+++ app/pages/global-settings/global-settings.component.js
@@ -1,8 +1,14 @@
 
 import language from '../../core/services/language.js';
+import storage from '../../core/services/storage';
 
 export default class GlobalSettingsController {
 	constructor() {
 		this.language = language.getCurrentName();
+		this.advanced = storage.getItem('advanced');
+	}
+
+	toggleAdvanced() {
+		storage.setItem('advanced', this.advanced);
 	}
 }
