f91344f36f9375334bfce9528dba2f08dce59592
Johan Stén <johan.sten@gmail.com>
2018-03-16 08:15:06 +0800

    Hide "delete account" unless advanced mode is on

--- app/pages/account-settings/advanced-settings.component.js
+++ app/pages/account-settings/advanced-settings.component.js
@@ -1,10 +1,14 @@
 
+import storage from '../../core/services/storage';
+
 export default class AdvancedSettingsController {
 
 	/* @ngInject */
 	constructor(Reverse, Wallet) {
 
 		this.account = Wallet.current;
+
+		this.advanced = storage.getItem('advanced');
 
 		if ('signers' in this.account) {
 			this.hasOtherSigners = this.account.signers.length !== 1;
