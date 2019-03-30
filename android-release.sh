83347e5e68aa0ac8975e1a9cf7b395226c671b60
Johan Stén <johan@futuretense.io>
2019-03-30 20:16:01 +0800

    Update release script

--- android-release.sh
+++ android-release.sh
@@ -5,4 +5,4 @@ rm Stargazer.apk
 ionic cordova build android --release
 cp platforms/android/build/outputs/apk/android-release-unsigned.apk .
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
-~/Library/Android/sdk/build-tools/24.0.2/zipalign -v 4 android-release-unsigned.apk Stargazer.apk
+~/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 android-release-unsigned.apk Stargazer.apk
